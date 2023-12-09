cd4c79fa65f3fddecdbb60bebe9efdb748160ace
Pete Keen <pete@petekeen.net>
2023-12-08 20:09:10 -0500

    8.2

--- day_08.rb
+++ day_08.rb
@@ -5,14 +5,20 @@ module Day08
     const :path, T::Array[Integer]
     const :nodes, T::Array[T::Array[Integer]]
 
+    const :start_nodes, T::Array[Integer]
+    const :end_nodes, T::Array[Integer]
+
     def self.parse(input)
       path_str, nodes_str = input.strip.split(/\n\n/, 2)
       path = path_str.strip.split('').map { |p| p == 'L' ? 0 : 1 }
 
       node_index = {}
       raw_nodes = []
+      start_nodes = []
+      end_nodes = []
 
       node_regex = /(\w{3}) = \((\w{3}), (\w{3})\)/
+
       nodes_str.strip.split(/\n/).sort.each do |node|
         m = node.match(node_regex)
         raise "no match" if m.nil?
@@ -24,7 +30,15 @@ module Day08
         log(n: n, l: l, r: r)
 
         raw_nodes << [l, r]
-        node_index[n] = raw_nodes.length - 1
+        insert_index = raw_nodes.length - 1
+
+        if n.end_with?('A')
+          start_nodes << insert_index
+        elsif n.end_with?('Z')
+          end_nodes << insert_index
+        end
+
+        node_index[n] = insert_index
       end
 
       nodes = []
@@ -35,6 +49,8 @@ module Day08
       new(
         path: path,
         nodes: nodes,
+        start_nodes: start_nodes,
+        end_nodes: end_nodes
       )
     end
 
@@ -47,31 +63,74 @@ module Day08
       while count < 100000
         break if cur == nodes.length - 1
         path.each do |path|
-#          log(op: :before, count: count, cur: cur, path: path)
-
           count += 1
           cur = nodes[cur][path]
-
-#          log(op: :after, count: count, cur: cur, path: path, l: nodes.length)
           break if cur == nodes.length - 1
         end
       end
 
       count
     end
+
+    def part_two!
+      counts = []
+
+      start_nodes.each do |node|
+        count = 0
+
+        cur = node
+
+        while count < 100000
+          break if end_nodes.include?(cur)
+          path.each do |path|
+            break if end_nodes.include?(cur)
+            count += 1
+            cur = nodes[cur][path]
+          end
+        end
+        counts << count
+      end
+      counts.reduce(1, :lcm)
+    end
   end
 
-  class Test < Minitest::Test
+  class TestOne < Minitest::Test
     def setup
       @example, @input = read_data('08')
     end
 
-    def test_part_one_example
-      assert_equal(6, Graph.parse(@example).part_one!)
+    # def test_example
+    #   assert_equal(6, Graph.parse(@example).part_one!)
+    # end
+
+    # def test_input
+    #   assert_equal(19241, Graph.parse(@input).part_one!)
+    # end
+  end
+
+  class TestTwo < Minitest::Test
+    def setup
+      _, @input = read_data('08')
+      @example = <<-HERE
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+      HERE
     end
 
-    def test_part_one_input
-      assert_equal(19241, Graph.parse(@input).part_one!)
+    def test_example
+      assert_equal(6, Graph.parse(@example).part_two!)
+    end
+
+    def test_input
+      assert_equal(-1, Graph.parse(@input).part_two!)
     end
   end
 end
